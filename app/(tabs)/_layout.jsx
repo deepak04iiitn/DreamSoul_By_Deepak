e86b80e601c325fa3ba2fd2b5c34ce3e20f3069c
Deepak Kumar Yadav <dky422003@gmail.comm>
2025-06-08 14:40:10 +0530

    made icons

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,16 +1,46 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import { Colors } from '../../assets/Colors'
+import Ionicons from '@expo/vector-icons/Ionicons'
 
 const TabLayout = () => {
   return (
-    <Tabs screenOptions={{headerShown:false , tabBarActiveTintColor:}}>
-        <Tabs.Screen name='home' options={{title:"Home"}} />
-        <Tabs.Screen name='pair' options={{title:"Pair"}} />
-        <Tabs.Screen name='post' options={{title:"Post"}} />
-        <Tabs.Screen name='chats' options={{title:"Chats"}} />
-        <Tabs.Screen name='profile' options={{title:"Profile"}} />
+    <Tabs screenOptions={{headerShown: false , tabBarActiveTintColor: Colors.PRIMARY , 
+        tabBarInactiveTintColor: Colors.dark.text ,
+        tabBarStyle: {
+            backgroundColor: Colors.SECONDARY,
+            paddingBottom: 14,
+            height: 75
+        },
+        tabBarLabelStyle: {
+            fontSize: 12,
+            fontWeight: "bold"
+        }
+    }}>
+
+        <Tabs.Screen name='home' options={{title:"Home" , tabBarIcon: ({color}) => (
+            <Ionicons name='home' size={24} color={color}/>
+        )}} />
+
+        <Tabs.Screen name='pair' options={{title:"Pair" , tabBarIcon: ({color}) => (
+            <Ionicons name="heart-circle-sharp" size={24} color={color} />
+        )}} />
+
+        <Tabs.Screen name='post' options={{title:"Post" , tabBarIcon: ({color}) => (
+            <Ionicons name="add-circle-sharp" size={24} color={color} />
+        )}} />
+
+        <Tabs.Screen name='chats' options={{title:"Chats" , tabBarIcon: ({color}) => (
+            <Ionicons name="chatbubble-ellipses-sharp" size={24} color={color} />
+        )}} />
+
+        <Tabs.Screen name='profile' options={{title:"Profile" , tabBarIcon: ({color}) => (
+            <Ionicons name="person-circle-sharp" size={24} color={color} />
+        )}} />
+
         <Tabs.Screen name='notifications' options={{title:"Notifications"}} />
+
     </Tabs>
   )
 }
